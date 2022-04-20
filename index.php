aa62add7cd1a0f6f075667b0b92b69dbbd766a3f
linkli <linkli@umich.edu>
2022-04-19 22:04:11 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<input type="text" placeholder="Search Image..">
+
+<table width="80%">
+    <tr>
+        <td>Image</td>
+        <td>Title</td>
+        <td>Owner</td>
+        <td width="50%">Description</td>
+        <td>Favorites</td>
+        <td>Flickr page</td>
+
+    </tr>
+</table>
